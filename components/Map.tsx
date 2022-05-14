881ec4bebdc38e6bade6d17e4285438935a15981
asgerrud <asgerrud@gmail.com>
2022-05-14 23:54:13 +0200

    Add initial map view

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,31 +1,42 @@
+import { FC } from 'react';
+import * as ReactDOMServer from 'react-dom/server';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'
 
+type MapProps = {
+  darkMode: boolean
+}
 
-let boulderIcon = L.icon({
-  iconUrl: '../assets/images/boulder.png',
-  iconRetinaUrl: '../assets/images/boulder.png',
+const icon = (filename) => L.icon({
+  iconUrl: `../assets/images/${filename}`,
+  iconRetinaUrl: `../assets/images/${filename}`,
   iconSize: [25, 25]
-});
+})
 
-const Map = () => {
+const boulderIcon = icon("boulder.png");
+const indoorBoulderIcon = icon("bouldering-gym.png");
+const indoorRopeIcon = icon("indoor-rope.png");
+
+const Map: FC<MapProps> = ({ darkMode }) => {
 
   const tileLayerLight = `https://api.mapbox.com/styles/v1/asger-rud/cl3621cy8000314mpq30pbmh2/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`;
   const tileLayerDark = `https://api.mapbox.com/styles/v1/asger-rud/cl35zxnpz000y14lay3sj3htb/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`
 
   return (
     <>
-      <MapContainer center={[55.662622, 12.568025]} zoom={13} style={{height: 400, width: "100%"}}>
+      <MapContainer center={[55.662622, 12.568025]} zoom={12} style={{height: "85vh", width: "100%"}}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url={tileLayerLight}
+          url={darkMode ? tileLayerDark : tileLayerLight}
         />
-        <Marker icon={boulderIcon} position={[55.641149, 12.539375]}>
+        {/* <Marker icon={L.divIcon({ className: "custom icon", html: ReactDOMServer.renderToString( <MyComponent/> ) })} position={[55.641149, 12.539375]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
+        </Marker> */}
+        <Marker icon={indoorRopeIcon} position={[55.69283658847049, 12.610710054044832]} />
+        <Marker icon={boulderIcon} position={[55.621510106227085, 12.544120861630542]} />
       </MapContainer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,28 @@
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import dynamic from 'next/dynamic'
 import Hero from '../components/Hero.tsx'
-import Image from 'next/image'
+import { Button, Center } from '@chakra-ui/react'
 
 export default function Home() {
   
+  const [darkMode, setDarkMode] = useState(false)
+
   const Map = useMemo(() => dynamic(() => import('../components/Map.tsx'), // replace '@components/map' with your component's location
     { 
       loading: () => <p>Loading...</p>,
       ssr: false 
     } // This line is important. It's what prevents server-side render
-  ), [])
+  ), [darkMode])
 
   return (
     <div>
+      <Map darkMode={darkMode} variant='ghost' />
+      <Center p={4} mx="auto">
+        <Button onClick={() => setDarkMode(!darkMode)}>
+          <span>{darkMode ? '😏' : '😭'}</span>
+        </Button>
+      </Center>
       <Hero />
-      <Map />
     </div>
   )
 }
