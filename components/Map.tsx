e6e33f8f5a0b42b7941f638189aa0ca1edf4c1f4
asgerrud <asgerrud@gmail.com>
2022-05-14 17:55:44 +0200

    Create map object and marker

---
+++ components/Map.tsx
@@ -0,0 +1,31 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css'
+
+
+let boulderIcon = L.icon({
+  iconUrl: '../assets/images/boulder.png',
+  iconRetinaUrl: '../assets/images/boulder.png',
+  iconSize: [25, 25]
+});
+
+const Map = () => {
+
+  return (
+    <>
+      <MapContainer center={[55.662622, 12.568025]} zoom={13} style={{height: 400, width: "100%"}}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://api.mapbox.com/styles/v1/asger-rud/cl35zxnpz000y14lay3sj3htb/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}` }
+        />
+        <Marker icon={boulderIcon} position={[55.641149, 12.539375]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  )
+}
+
+export default Map;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useMemo } from 'react'
+import dynamic from 'next/dynamic'
 import Hero from '../components/Hero.tsx'
+import Image from 'next/image'
 
 export default function Home() {
+  
+  const Map = useMemo(() => dynamic(() => import('../components/Map.tsx'), // replace '@components/map' with your component's location
+    { 
+      loading: () => <p>Loading...</p>,
+      ssr: false 
+    } // This line is important. It's what prevents server-side render
+  ), [])
+
   return (
     <div>
       <Hero />
+      <Map />
     </div>
   )
 }
