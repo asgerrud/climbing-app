ad7b394b0f71a4b6ae0b09b07ddf04b831addcf8
asgerrud <asgerrud@gmail.com>
2022-05-26 23:30:38 +0200

    Sort notes by newest

--- components/profile/NoteGrid.tsx
+++ components/profile/NoteGrid.tsx
@@ -9,9 +9,11 @@ type NoteGridProps = {
 
 const NoteGrid: React.FC<NoteGridProps> = ({ notes }) => {
   
+  const sortByDate = (a: Date,b: Date) => new Date(b).getTime() - new Date(a).getTime()
+
   return (
     <VStack mt={4} spacing={2} alignItems="start">
-      { notes.map(note => <Note key={note.id} note={note} />) }
+      { notes.sort((a, b) => sortByDate(a.created_at, b.created_at)).map(note => <Note key={note.id} note={note} />) }
     </VStack>
   )
 }
