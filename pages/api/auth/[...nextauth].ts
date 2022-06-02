0d279697c5715c8b62dabe7830768e8da0cc7409
asgerrud <asgerrud@gmail.com>
2022-06-02 21:00:06 +0200

    Add Google as authentication provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { NextApiHandler } from 'next'
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 import prisma from '../../../lib/prisma'
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
@@ -12,6 +13,10 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
