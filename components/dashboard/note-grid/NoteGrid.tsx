6cb8af48fc0275aad76c90803538627a1d1c7644
asgerrud <asgerrud@gmail.com>
2022-06-25 18:02:38 +0200

    Update NoteGrid.tsx

--- components/dashboard/note-grid/NoteGrid.tsx
+++ components/dashboard/note-grid/NoteGrid.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { VStack } from '@chakra-ui/react'
 import { Note as NoteType } from '@prisma/client'
 import Note from '../note/Note'
-import { sortByDate } from '../../../utils/sort';
+import { sortByDate } from '../../../utils/sort'
 
 type NoteGridProps = {
   notes: NoteType[]
