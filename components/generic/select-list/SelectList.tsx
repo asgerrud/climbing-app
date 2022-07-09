65c117bb3aede6be8b56160426104df6caa9eb80
asgerrud <asgerrud@gmail.com>
2022-07-09 20:34:32 +0200

    Create SelectList component

---
+++ components/generic/select-list/SelectList.tsx
@@ -0,0 +1,43 @@
+import { Button, HStack, Select, StackProps } from '@chakra-ui/react'
+import { useState } from 'react'
+
+type SelectListProps = {
+  options: any[]
+  styleProps: StackProps
+}
+
+const SelectList: React.FC<SelectListProps> = ({ options, ...styleProps }) => {
+  
+  const [currentOption, setCurrentOption] = useState('')
+  const [optionsSelected, setOptionsSelected ] = useState<any[]>([])
+
+  const onAddOption = () => {
+    const options = optionsSelected
+    if (currentOption == '') return 
+    if (optionsSelected.includes(currentOption) == false) {
+      options.push(currentOption)
+      setOptionsSelected([...options])
+      setCurrentOption('')
+    }
+  }
+
+  return (
+    <>
+        <HStack spacing={2} {...styleProps} >
+          <Select value={currentOption} onChange={(e) => setCurrentOption(e.target.value)}>
+            <option value="">Select option</option>
+            {options.filter(option => optionsSelected.includes(option) == false).map((option, _idx) => {
+              return <option key={_idx} value={option}>{option}</option>
+            })}
+          </Select>
+          <Button onClick={onAddOption}>Add facility</Button>
+        </HStack>
+      {optionsSelected.map((option, _idx) => {
+        return <p key={_idx}>{option}</p>
+      })}
+    </>
+  )
+}
+
+export default SelectList
+
