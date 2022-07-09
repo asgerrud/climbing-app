7dc475593db32ab0f44b4ffb860f5bcac929e678
asgerrud <asgerrud@gmail.com>
2022-07-09 21:05:25 +0200

    Add event handler function

--- components/generic/select-list/SelectList.tsx
+++ components/generic/select-list/SelectList.tsx
@@ -1,39 +1,43 @@
-import { Button, HStack, Select, StackProps } from '@chakra-ui/react'
+import { Button, HStack, Select, Text } from '@chakra-ui/react'
 import { useState } from 'react'
 
 type SelectListProps = {
   options: string[]
+  onOptionAdded?: (o: string[]) => void
   [x:string]: any
 }
 
-const SelectList: React.FC<SelectListProps> = ({ options, ...styleProps }) => {
+const SelectList: React.FC<SelectListProps> = ({ options, onOptionAdded, ...styleProps }) => {
   
   const [currentOption, setCurrentOption] = useState('')
-  const [optionsSelected, setOptionsSelected ] = useState<any[]>([])
+  const [optionsSelected, setOptionsSelected ] = useState<string[]>([])
 
-  const onAddOption = () => {
+  const handleOptionAdded = () => { onOptionAdded(options) }
+
+  const addOption = () => {
     const options = optionsSelected
     if (currentOption == '') return 
     if (optionsSelected.includes(currentOption) == false) {
       options.push(currentOption)
       setOptionsSelected([...options])
       setCurrentOption('')
+      handleOptionAdded
     }
   }
 
   return (
     <>
-        <HStack spacing={2} {...styleProps} >
+        <HStack spacing={2} {...styleProps} mb={2} >
           <Select value={currentOption} onChange={(e) => setCurrentOption(e.target.value)}>
             <option value="">Select option</option>
             {options.filter(option => optionsSelected.includes(option) == false).map((option, _idx) => {
               return <option key={_idx} value={option}>{option}</option>
             })}
           </Select>
-          <Button onClick={onAddOption}>Add facility</Button>
+          <Button onClick={addOption}>Add facility</Button>
         </HStack>
       {optionsSelected.map((option, _idx) => {
-        return <p key={_idx}>{option}</p>
+        return <Text key={_idx} my={1}>{option}</Text>
       })}
     </>
   )
